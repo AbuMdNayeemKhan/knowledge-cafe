6bdab84adb66f74bc23f3217f10a26a937b39524
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-03-16 21:30:46 +0600

    start codeing

---
+++ src/assets/Components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-4xl font-bold bg-red-300 p-5'>Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
